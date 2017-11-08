ef89356b3a5427c4fd016d4a293bf20d57126fa4
Daniel Schell <daniel.schell@kit.edu>
2017-11-08 11:02:44 +0100

    Move EUTelTrackerDataInterfacer definitions

---
+++ eutelescope/interfaces/pixel/src/EUTelTrackerDataInterfacer.cc
@@ -0,0 +1,24 @@
+#include "EUTelTrackerDataInterfacer.h"
+
+using namespace eutelescope;
+
+std::vector<std::reference_wrapper<EUTelBaseSparsePixel const>> const &
+EUTelTrackerDataInterfacer::getPixels() const {
+	if (!_refVecValid) {
+		this->validateRefVec();
+	}
+	return _refVec;
+}
+
+auto EUTelTrackerDataInterfacer::begin() const {
+      if (!_refVecValid)
+        this->validateRefVec();
+      return _refVec.cbegin();
+    }
+
+auto EUTelTrackerDataInterfacer::end() const {
+      if (!_refVecValid)
+        this->validateRefVec();
+      return _refVec.cend();
+    }
+
